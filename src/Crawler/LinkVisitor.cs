de9aa321f3be4d4ae21dd4db6a29848007d3e81f
Khang <41323253+KhangWoei@users.noreply.github.com>
2025-06-14 12:12:58 +0100

    wip - extract visitation logic to a type

---
+++ src/Crawler/LinkVisitor.cs
@@ -0,0 +1,19 @@
+using System.Net.Http.Headers;
+
+namespace Crawler;
+
+internal static class LinkVisitor
+{
+    public static async Task<string> VisitAsync(Uri uri, CancellationToken cancellationToken)
+    {   
+        var client = new HttpClient();
+        var response = await client.GetAsync(uri, cancellationToken);
+        
+        if (response.IsSuccessStatusCode && response.Content.Headers.ContentType?.MediaType != "html")
+        {
+            return await client.GetStringAsync(uri, cancellationToken);
+        }
+
+        return string.Empty;
+    }
+}
