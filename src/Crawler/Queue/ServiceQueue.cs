ff5a45ee1cd578ede14a97c3ffe72ee104c6a184
kwc43 <41323253+kwc43@users.noreply.github.com>
2025-06-25 23:24:08 +0100

    wip

---
+++ src/Crawler/Queue/ServiceQueue.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Channels;
+
+namespace Crawler.Queue;
+
+internal sealed class ServiceQueue
+{
+    private readonly Channel<CrawlSource> _channel;
+
+    public ServiceQueue()
+    {
+        _channel = Channel.CreateUnbounded<CrawlSource>();
+    }
+
+    public async ValueTask EnqueueAsync(CrawlSource message)
+    {
+        ArgumentNullException.ThrowIfNull(message);   
+        
+        await _channel.Writer.WriteAsync(message);
+    }
+
+    public async ValueTask<CrawlSource> DequeueAsync(CancellationToken cancellationToken)
+    {
+        return await _channel.Reader.ReadAsync(cancellationToken);
+    }
+
+    public int Count() => _channel.Reader.Count;
+}
---
+++ src/Crawler/Queue/Worker.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Crawler.Queue;
+
+internal sealed class Worker(ServiceQueue queue): BackgroundService
+{
+    private readonly SemaphoreSlim _semaphore = new(Environment.ProcessorCount);
+    private readonly HashSet<Guid> _inProgress = new();
+    
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested && _inProgress.Count > 0 && queue.Count() > 0)
+        {
+            try
+            {
+                var work = await queue.DequeueAsync(cancellationToken);
+                
+                await _semaphore.WaitAsync(cancellationToken);
+                
+                var taskId = new Guid();
+                lock (_inProgress)
+                {
+                    _inProgress.Add(taskId);
+                }
+                _ = Execute(taskId, work, cancellationToken);
+
+            }
+            catch (OperationCanceledException) { }
+            
+        }
+    }
+
+    private async Task Execute(Guid taskId, CrawlSource work, CancellationToken cancellationToken)
+    {
+        
+        
+        
+        _semaphore.Release();
+        lock (_inProgress)
+        {
+            _inProgress.Remove(taskId);
+        }
+    }
+}
